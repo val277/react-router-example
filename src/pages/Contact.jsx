9f20f46a25e25b14422f2774e218b7fc4ffd3c72
Valentin <218587221+val277@users.noreply.github.com>
2025-09-10 15:37:25 +0200

    edited contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Contact() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      email: e.target.email.value,
+      message: e.target.message.value,
+    });
+    e.target.reset();
+    setIsSubmitted(true);
+  };
+
   return (
     <main>
       <h1>Contact us</h1>
-      <form action="oui">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="message">Message</label>
-        <textarea
-          name="message"
-          id="message"
-          rows="10"
-          cols="50"
-          placeholder="Enter your message"
-        ></textarea>
-        <input type="submit" value="Send" />
-      </form>
+      {isSubmitted ? (
+        <>
+          <p style={{ color: "green" }}>Message sent</p>
+          <Link to="/">→ Go to Home</Link>
+        </>
+      ) : (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            rows="10"
+            cols="50"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <input type="submit" value="Send" />
+        </form>
+      )}
     </main>
   );
 }
